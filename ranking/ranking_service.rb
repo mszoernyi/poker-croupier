4d4e6e3193eb2d0ee7af267cca92851745b548ac
devill <devillmeister@gmail.com>
2014-02-03 21:44:58 +0100

    #36 - Removed ranking service (It will have to be reimplemented anyway...)

--- ranking/ranking_service.rb
+++
@@ -1,6 +0,0 @@
-$:.push(File.join(File.dirname(__FILE__), '../common/lib'))
-
-require 'thrift_builder'
-require_relative 'handler'
-
-ThriftBuilder.server(API::Ranking::Processor, Ranking::Handler, 9080).serve()

1530e141c5ac0748daf4b4c89b7883cbd9478434
devill <devillmeister@gmail.com>
2014-02-04 20:27:07 +0100

    Build fix

--- croupier/croupier.rb
+++ croupier/croupier.rb
@@ -2,7 +2,6 @@ $:.push(File.join(File.dirname(__FILE__), 'lib/api'))
 $:.push(File.join(File.dirname(__FILE__)))
 
 require 'thrift'
-require_relative 'lib/api/croupier'
 require_relative '../common/lib/delegate_all'
 
 module Croupier

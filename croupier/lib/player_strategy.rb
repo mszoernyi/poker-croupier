882ad248c8135886e2dc518846018fdaf24e01fe
devill <devillmeister@gmail.com>
2014-02-07 21:08:59 +0100

    Removed ThriftObserver

--- croupier/lib/player_strategy.rb
+++ croupier/lib/player_strategy.rb
@@ -1,9 +1,13 @@
 require 'player_strategy'
 
-class Croupier::PlayerStrategy < Croupier::ThriftObserver
+class Croupier::PlayerStrategy
+  attr_reader :gateway
+  attr_reader :strategy
 
   def initialize(strategy, transport)
-    super(strategy, transport)
+    @strategy = strategy
+    @transport = transport
+    @gateway = Croupier::ThriftEntityGateway
   end
 
   def hole_card(card)
@@ -12,6 +16,42 @@ class Croupier::PlayerStrategy < Croupier::ThriftObserver
 
   def bet_request(pot, hash)
     strategy.bet_request pot, gateway.bet_limits(hash)
+  end
+
+  def open
+    @transport.open
+  end
+
+  def close
+    @transport.close
+  end
+
+  def competitor_status(competitor)
+    @strategy.competitor_status @gateway[competitor]
+  end
+
+  def name
+    @name ||= @strategy.name
+  end
+
+  def bet(competitor, bet)
+    @strategy.bet(@gateway[competitor], @gateway.bet(bet))
+  end
+
+  def community_card(card)
+    @strategy.community_card @gateway[card]
+  end
+
+  def showdown(competitor, hand)
+    @strategy.showdown @gateway[competitor], competitor.hole_cards.map { |card| @gateway[card] }, @gateway[hand]
+  end
+
+  def winner(competitor, amount)
+    @strategy.winner @gateway[competitor], amount
+  end
+
+  def shutdown
+    @strategy.shutdown
   end
 
 end

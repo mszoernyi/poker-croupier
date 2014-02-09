df23ab6025813a4bb4ff2c880a082f191f7b36e0
devill <devillmeister@gmail.com>
2014-02-09 11:21:18 +0100

    First version of the rest player interface

--- croupier/lib/rest_player.rb
+++ croupier/lib/rest_player.rb
@@ -1,4 +1,7 @@
-require 'player_strategy'
+
+require 'net/http'
+require 'json'
+require 'uri'
 
 class Croupier::RestPlayer
 
@@ -6,12 +9,20 @@ class Croupier::RestPlayer
 
   def initialize(name, url)
     @name = name
+    uri = URI(url)
+    @host, @port, @path = uri.host, uri.port, uri.path
   end
 
   def hole_card(card)
   end
 
   def bet_request(game_state)
+    req = Net::HTTP::Post.new(@path)#, initheader = {'Content-Type' =>'application/json'})
+    req.set_form({ action: 'bet_request', game_state: game_state.to_json })
+    response = Net::HTTP.new(@host, @port).start {|http| http.request(req) }
+
+    return response.body.to_i if response.code == 200
+
     0
   end
 

eb06438664f7c43e769c10bb1722c754673c0910
devill <devillmeister@gmail.com>
2014-02-09 10:26:37 +0100

    Aesthetic change

--- croupier/scripts/integration_test_external_players.rb
+++ croupier/scripts/integration_test_external_players.rb
@@ -5,7 +5,7 @@ require_relative 'functions'
 number_of_players = (ARGV.length > 0) ? ARGV[0].to_i : 2
 
 sit_and_go "../../log/integration_test_java" do
-  for index in 0..number_of_players - 1
+  (0...number_of_players).each do |index|
     register_thrift_player("localhost:#{9200+index}")
   end
 end

0b2615e471e902e554b6fd0e955f52c632a25f89
devill <devillmeister@gmail.com>
2014-02-09 13:19:52 +0100

    Default PHP clent added to start script

--- croupier/scripts/start.rb
+++ croupier/scripts/start.rb
@@ -1,8 +1,5 @@
 require_relative 'functions'
 
 sit_and_go "../../log/game_#{run_timestamp}" do
-  register_thrift_player 'localhost:9200'
-  register_thrift_player 'localhost:9900'
-  register_thrift_player 'localhost:30303'
-  register_thrift_player 'http://localhost:8080/php/player_service.php'
+  register_rest_player "PHilip Pots", "http://localhost:8080/player_service.php"
 end

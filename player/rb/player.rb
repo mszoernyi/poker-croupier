51a8a1073ac8b52e95d4ca72c1101c7d63a52692
devill <devillmeister@gmail.com>
2014-02-09 12:15:58 +0100

    Removed thrift clients

--- player/rb/player.rb
+++
@@ -1,9 +0,0 @@
-$:.push(File.join(File.dirname(__FILE__), 'lib/api'))
-$:.push(File.join(File.dirname(__FILE__)))
-
-require 'thrift'
-require 'player_strategy'
-
-module PlayerStrategy
-  autoload :Handler, 'lib/handler'
-end
--- player/rb/player_service.rb
+++
@@ -1,6 +0,0 @@
-$:.push(File.join(File.dirname(__FILE__), '../../common/lib'))
-
-require 'thrift_builder'
-require_relative 'player'
-
-ThriftBuilder.server(API::PlayerStrategy::Processor, PlayerStrategy::Handler, ARGV[0] || 9200).serve()

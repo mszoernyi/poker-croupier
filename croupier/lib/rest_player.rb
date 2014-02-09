b5b4bafeeead4f0db3acbc83c9b1d542ffb0367c
devill <devillmeister@gmail.com>
2014-02-09 16:08:36 +0100

    #13: handle timeouts

--- croupier/lib/rest_player.rb
+++ croupier/lib/rest_player.rb
@@ -17,13 +17,20 @@ class Croupier::RestPlayer
   end
 
   def bet_request(game_state)
-    req = Net::HTTP::Post.new(@path)#, initheader = {'Content-Type' =>'application/json'})
-    req.set_form({ action: 'bet_request', game_state: game_state.to_json })
-    response = Net::HTTP.new(@host, @port).start {|http| http.request(req) }
+    begin
+      req = Net::HTTP::Post.new(@path)
+      req.set_form({ action: 'bet_request', game_state: game_state.to_json })
+      http_connection = Net::HTTP.new(@host, @port)
+      http_connection.open_timeout = 0.5
+      http_connection.read_timeout = 0.5
+      response = http_connection.start {|http| http.request(req) }
 
-    return 0 unless response.code.to_i == 200
+      return 0 unless response.code.to_i == 200
 
-    response.body.to_i
+      response.body.to_i
+    rescue
+      return 0
+    end
   end
 
   def open

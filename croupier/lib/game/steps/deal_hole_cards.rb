3e4aa4a86bacdb5003f672c14142b473be278146
devill <devillmeister@gmail.com>
2014-02-16 10:40:24 +0100

    Log each hole card deal separately

--- croupier/lib/game/steps/deal_hole_cards.rb
+++ croupier/lib/game/steps/deal_hole_cards.rb
@@ -10,8 +10,9 @@ class Croupier::Game::Steps::DealHoleCards < Croupier::Game::Steps::Base
   def deal_one_card_to_each_player
     game_state.each_player_from game_state.first_player do |player|
       if player.has_stack?
-        player.hole_card(game_state.deck.next_card!)
-        game_state.log_state
+        card = game_state.deck.next_card!
+        player.hole_card(card)
+        game_state.log_state "#{player.name} recieved hole_card #{card}"
       end
     end
   end

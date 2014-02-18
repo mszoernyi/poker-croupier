867783ba77c8710ea6b4373c6cd40b7667ef2558
devill <devillmeister@gmail.com>
2014-02-18 22:45:38 +0100

    Removed specators from tournament state maker

--- croupier/spec/spec_helper/make_tournament_state.rb
+++ croupier/spec/spec_helper/make_tournament_state.rb
@@ -2,17 +2,9 @@ class SpecHelper::MakeTournamentState
   def self.with(options)
     game_state = Croupier::Tournament::State.new
     players = options[:players] || []
-    spectators = options[:logger] || []
 
     players.each do |player|
       game_state.register_player(player)
-    end
-    if spectators.length > 0
-      spectators.each do |spectator|
-        game_state.set_logger(spectator)
-      end
-    else
-      game_state.set_logger SpecHelper::DummyClass.new
     end
 
     game_state

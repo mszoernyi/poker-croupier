31c0c0b1ab8e95a42bfd99e1e940d5ab1d3d8002
devill <devillmeister@gmail.com>
2014-02-20 21:43:11 +0100

    Added java client to the start script

--- croupier/scripts/start.rb
+++ croupier/scripts/start.rb
@@ -6,4 +6,7 @@ sit_and_go "../../log/game_#{run_timestamp}" do
 
   # Ruby client
   register_rest_player "Rudy Ruby", "http://localhost:4567/"
+
+  # Java client
+  register_rest_player "Jim Java", "http://localhost:8080/"
 end

e1a83aefe4c00970b8f286962e5ac0b476b4a025
devill <devillmeister@gmail.com>
2014-02-20 22:39:27 +0100

    Updated start rb to reflect port changes in players

--- croupier/scripts/start.rb
+++ croupier/scripts/start.rb
@@ -2,10 +2,10 @@ require_relative 'functions'
 
 sit_and_go "../../log/game_#{run_timestamp}" do
   # PHP client
-  register_rest_player "PHilip Pots", "http://localhost:8080/player_service.php"
+  register_rest_player "PHilip Pots", "http://localhost:8000/player_service.php"
 
   # Ruby client
-  register_rest_player "Rudy Ruby", "http://localhost:4567/"
+  register_rest_player "Rudy Ruby", "http://localhost:8090/"
 
   # Java client
   register_rest_player "Jim Java", "http://localhost:8080/"

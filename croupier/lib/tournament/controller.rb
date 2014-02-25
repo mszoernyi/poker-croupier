bf2dd6a05d70d3c4aa74e8c3e15680abd4d2df73
devill <devillmeister@gmail.com>
2014-02-25 23:06:01 +0100

    Run multiple sit and go games

--- croupier/lib/tournament/controller.rb
+++ croupier/lib/tournament/controller.rb
@@ -1,4 +1,23 @@
 
 class Croupier::Tournament::Controller
+  def initialize
+    @players = []
+  end
 
+  def register_rest_player(name, strategy)
+    @players << { name: name, strategy: strategy }
+  end
+
+  def start_tournament(options)
+    iterations_left = options[:max_iterations] || -1
+    while iterations_left != 0
+      sit_and_go_controller = Croupier::SitAndGo::Controller.new
+      @players.each do |player|
+        sit_and_go_controller.register_rest_player player[:name], player[:strategy]
+      end
+      sit_and_go_controller.start_sit_and_go
+
+      iterations_left -= 1
+    end
+  end
 end
--- croupier/spec/tournament/controller_spec.rb
+++ croupier/spec/tournament/controller_spec.rb
@@ -1,4 +1,44 @@
+require_relative '../spec_helper'
+
+
 
 describe Croupier::Tournament::Controller do
+
+  def register_test_player(name)
+    strategy = SpecHelper::DummyClass.new
+    subject.register_rest_player name, strategy
+    strategy
+  end
+
+  context "there are two players" do
+    let(:player1_strategy) { register_test_player('Player 1') }
+    let(:player2_strategy) { register_test_player('Player 2') }
+    let(:sit_and_go_controller_mock) { double }
+
+    before :each do
+      Croupier::SitAndGo::Controller.stub(:new).and_return sit_and_go_controller_mock
+    end
+
+    def expect_a_round_of_sit_and_go
+      sit_and_go_controller_mock.should_receive(:register_rest_player).with('Player 1', player1_strategy)
+      sit_and_go_controller_mock.should_receive(:register_rest_player).with('Player 2', player2_strategy)
+      sit_and_go_controller_mock.should_receive(:start_sit_and_go)
+    end
+
+    it "should run a sit and go" do
+      expect_a_round_of_sit_and_go
+
+      subject.start_tournament max_iterations: 1
+    end
+
+    it "should run the sit and go multiple times" do
+      expect_a_round_of_sit_and_go
+      expect_a_round_of_sit_and_go
+
+      subject.start_tournament max_iterations: 2
+    end
+  end
+
+
 
 end

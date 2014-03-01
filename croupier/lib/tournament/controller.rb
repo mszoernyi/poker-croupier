6cdee18ba6f8c3a89872909577e81aa021d28370
devill <devillmeister@gmail.com>
2014-03-01 22:05:47 +0100

    Refactor

--- croupier/lib/tournament/controller.rb
+++ croupier/lib/tournament/controller.rb
@@ -16,18 +16,27 @@ class Croupier::Tournament::Controller
   def start_tournament
 
     sit_and_go_controller = Croupier::SitAndGo::Controller.new
+
+    start_players(sit_and_go_controller)
+
+    sleep(2)
+    sit_and_go_controller.start_sit_and_go
+
+    stop_players
+
+  end
+
+  def stop_players
+    @players.each do |player|
+      Process.spawn("bash #{player[:directory]}/stop.sh")
+    end
+  end
+
+  def start_players(sit_and_go_controller)
     @players.each do |player|
       config = YAML.load_file("#{player[:directory]}/config.yml")
       Process.spawn("bash #{player[:directory]}/start.sh")
       sit_and_go_controller.register_rest_player player[:name], config["url"]
     end
-
-    sleep(2)
-    sit_and_go_controller.start_sit_and_go
-
-    @players.each do |player|
-      Process.spawn("bash #{player[:directory]}/stop.sh")
-    end
-
   end
 end

00768d200cc69b80342a2420b937bf504db05a3e
devill <devillmeister@gmail.com>
2014-03-08 17:04:01 +0100

    Added CPP player

--- croupier/scripts/start_sit_and_go.rb
+++ croupier/scripts/start_sit_and_go.rb
@@ -1,20 +1,22 @@
 require_relative 'functions'
 
 sit_and_go "../../log/game_#{run_timestamp}" do
-  # PHP client
-  register_rest_player "PHilip Pots", "http://localhost:8000/player_service.php"
 
-  # Ruby client
-  register_rest_player "Rudy Ruby", "http://localhost:8090/"
+  # C++ client
+  register_rest_player "Cecil P. Plush", "http://localhost:9300/"
 
   # Java client
   register_rest_player "Jim Java", "http://localhost:8080/"
 
   # JavaScript client
-  register_rest_player "Jim Java", "http://localhost:1337/"
+  register_rest_player "Javier Scrivo", "http://localhost:1337/"
+
+  # PHP client
+  register_rest_player "PHilip Pots", "http://localhost:8000/player_service.php"
 
   # Python client
   register_rest_player "Peter Python", "http://localhost:9000/"
 
-
+  # Ruby client
+  register_rest_player "Rudy Ruby", "http://localhost:8090/"
 end
--- croupier/scripts/start_tournament.rb
+++ croupier/scripts/start_tournament.rb
@@ -7,9 +7,10 @@ tournament do
   tournament_logfile "#{CROUPIER_ROOT}/log/tournament_#{run_date}"
   sit_and_go_logfile "#{CROUPIER_ROOT}/log/game_#{run_timestamp}"
 
-  register_git_player "John Script", "#{PLAYERS_ROOT}/poker-player-js"
-  register_git_player "Rudy Ruby", "#{PLAYERS_ROOT}/poker-player-ruby"
+  register_git_player "Cecil P. Plush", "#{PLAYERS_ROOT}/poker-player-cpp"
+  register_git_player "Jim Java", "#{PLAYERS_ROOT}/poker-player-java"
+  register_git_player "Javier Scrivo", "#{PLAYERS_ROOT}/poker-player-js"
   register_git_player "PHilip Pots", "#{PLAYERS_ROOT}/poker-player-php"
   register_git_player "Peter Python", "#{PLAYERS_ROOT}/poker-player-python"
-  register_git_player "Jim Java", "#{PLAYERS_ROOT}/poker-player-java"
+  register_git_player "Rudy Ruby", "#{PLAYERS_ROOT}/poker-player-ruby"
 end

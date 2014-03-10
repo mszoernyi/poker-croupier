78d957d4d101c6b289fdb80dd65d67470f13d1bf
devill <devillmeister@gmail.com>
2014-03-10 22:35:45 +0100

    #46 - Instead of separate page preload tweets into the game page

--- visual_spectator/visual_spectator.rb
+++ visual_spectator/visual_spectator.rb
@@ -14,25 +14,6 @@ get "/game" do
   Game.new(request[:log]).render
 end
 
-get "/tweets" do
-  config_file = File.dirname(__FILE__) + '/twitter_api.yml'
-  return "[]" unless FileTest.exist? config_file
-
-  twitter_config = YAML.load(File.open(config_file).read)
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key    = twitter_config['key']
-    config.consumer_secret = twitter_config['secret']
-  end
-
-  JSON.generate(client.search(twitter_config['search'], rpp: 10, result_type:'recent').take(10).map do |tweet|
-    {
-        profile_image: tweet.user.profile_image_url.to_s,
-        username: tweet.user.username,
-        text: tweet.text
-    }
-  end)
-end
-
 BASE_DIR = File.dirname(__FILE__)+"/../"
 LOG_DIR = File.dirname(__FILE__)+"/../log/"
 PREVIOUS_EVENTS_DIR = File.dirname(__FILE__)+"/../previous_events/"
@@ -58,5 +39,24 @@ class Game < MustacheBase
 
   def game_json
     File.read("#{BASE_DIR}#{@log_file}.json")
+  end
+
+  def tweets
+    config_file = File.dirname(__FILE__) + '/twitter_api.yml'
+    return "[]" unless FileTest.exist? config_file
+
+    twitter_config = YAML.load(File.open(config_file).read)
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key    = twitter_config['key']
+      config.consumer_secret = twitter_config['secret']
+    end
+
+    JSON.generate(client.search(twitter_config['search'], rpp: 10, result_type:'recent').take(10).map do |tweet|
+      {
+          profile_image: tweet.user.profile_image_url.to_s,
+          username: tweet.user.username,
+          text: tweet.text
+      }
+    end)
   end
 end

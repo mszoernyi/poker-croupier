7dd2b879bbd3109cdc6e25d8963dc4ce09c751ad
devill <devillmeister@gmail.com>
2014-03-23 19:56:40 +0100

    #48 - First version of the leader boards

--- visual_spectator/visual_spectator.rb
+++ visual_spectator/visual_spectator.rb
@@ -11,12 +11,20 @@ get "/" do
 end
 
 get "/game" do
-  Game.new(request[:log]).render
+  Game.new(request[:game_log]).render
+end
+
+get "/tournament" do
+  Tournament.new(request[:tournament_log]).render
 end
 
 BASE_DIR = File.dirname(__FILE__)+"/../"
 LOG_DIR = File.dirname(__FILE__)+"/../log/"
 PREVIOUS_EVENTS_DIR = File.dirname(__FILE__)+"/../previous_events/"
+
+def strip_extension(file)
+  File.dirname(file).split('/')[-1] + "/" + File.basename(file, ".*")
+end
 
 class MustacheBase < Mustache
   self.template_path = File.dirname(__FILE__)
@@ -24,13 +32,41 @@ end
 
 class List < MustacheBase
   def log_files
-    Dir.glob("#{LOG_DIR}*.json").sort.map { |file| { :file => File.basename(file, ".json") } }
+    Dir.glob("#{LOG_DIR}tournament_*.json").sort.map { |file| {:file => strip_extension(file)} }
   end
 
   def previous_events
-    Dir.glob("#{PREVIOUS_EVENTS_DIR}*/*.json").sort.map { |file| { :file => File.dirname(file).split('/')[-1] + "/" + File.basename(file, ".json") } }
+    Dir.glob("#{PREVIOUS_EVENTS_DIR}*/tournament_*.json").sort.map { |file| {:file => strip_extension(file)} }
   end
-end  
+end
+
+class Tournament < MustacheBase
+  def initialize(log)
+    @data = JSON.parse('[' + File.readlines("#{BASE_DIR}#{log}.json").join(',') + ']').reverse
+  end
+
+  def games
+    @data.map do |game|
+      game_winners = game_winners(game)
+      {
+          game_path: strip_extension(game['game_json']),
+          game_first: game_winners.sort_by { |player| player['place'] }[0]['name'],
+          game_second: game_winners.sort_by { |player| player['place'] }[1]['name'],
+          game_places: game_winners.sort_by { |player| player['place'] },
+          tournament_leader: game_winners.sort_by { |player| player['points'] }[-1]['name'],
+          tournament_leader_board: game_winners.sort_by { |player| -player['points'] }
+      }
+    end
+  end
+
+  def game_winners(game)
+    game_winners = []
+    game['ranking'].each_pair do |name, data|
+      game_winners << { 'name' => name }.merge(data)
+    end
+    game_winners
+  end
+end
 
 class Game < MustacheBase
   def initialize(log)
@@ -47,11 +83,11 @@ class Game < MustacheBase
 
     twitter_config = YAML.load(File.open(config_file).read)
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = twitter_config['key']
+      config.consumer_key = twitter_config['key']
       config.consumer_secret = twitter_config['secret']
     end
 
-    JSON.generate(client.search(twitter_config['search'], rpp: 10, result_type:'recent').take(10).map do |tweet|
+    JSON.generate(client.search(twitter_config['search'], rpp: 10, result_type: 'recent').take(10).map do |tweet|
       {
           profile_image: tweet.user.profile_image_url.to_s,
           username: tweet.user.username,

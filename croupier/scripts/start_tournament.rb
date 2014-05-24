ec709e96510b0b592cc09d5dc9345afad2daca9b
xza <xza@localhost>
2014-05-25 01:30:18 +0200

    start tournament with all currently available implementations of players

--- croupier/scripts/start_tournament.rb
+++ croupier/scripts/start_tournament.rb
@@ -7,11 +7,12 @@ tournament do
   tournament_logfile "#{CROUPIER_ROOT}/log/tournament_#{run_date}"
   sit_and_go_logfile "#{CROUPIER_ROOT}/log/game_#{run_timestamp}"
 
-  #register_git_player "Cecil P. Plush", "#{PLAYERS_ROOT}/poker-player-cpp"
-  #register_git_player "Jim Java", "#{PLAYERS_ROOT}/poker-player-java"
-  #register_git_player "Javier Scrivo", "#{PLAYERS_ROOT}/poker-player-js"
-  #register_git_player "Perl Jim", "#{PLAYERS_ROOT}/poker-player-perl"
-  register_git_player "PHilip Pots", "#{PLAYERS_ROOT}/poker-player-php"
-  register_git_player "Peter Python", "#{PLAYERS_ROOT}/poker-player-python"
-  register_git_player "Rudy Ruby", "#{PLAYERS_ROOT}/poker-player-ruby"
+  register_git_player "Cecil P. Plush", "#{PLAYERS_ROOT}/poker-player-cpp" # 9300
+  register_git_player "Groovy Gamer", "#{PLAYERS_ROOT}/poker-player-groovy" # 8180
+  register_git_player "His Pure Holiness", "#{PLAYERS_ROOT}/poker-player-haskell" # 1900
+  register_git_player "Jim Java", "#{PLAYERS_ROOT}/poker-player-java" # 8080
+  register_git_player "Javier Scrivo", "#{PLAYERS_ROOT}/poker-player-js" # 1337
+  register_git_player "PHilip Pots", "#{PLAYERS_ROOT}/poker-player-php" # 8000
+  register_git_player "Peter Python", "#{PLAYERS_ROOT}/poker-player-python" # 9000
+  register_git_player "Rudy Ruby", "#{PLAYERS_ROOT}/poker-player-ruby" # 8090
 end

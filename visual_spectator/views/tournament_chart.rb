49cba60c0f6760fab24af578067e6d792560b70a
xza <xza@localhost>
2014-05-25 09:44:23 +0200

    avoid NoMethodError in deploy_markers when previous_player is not known

--- visual_spectator/views/tournament_chart.rb
+++ visual_spectator/views/tournament_chart.rb
@@ -30,10 +30,10 @@ class TournamentChart < TournamentBase
     player_names.map do |name|
       this_player = game.player(name)
       previous_player = game.previous.player(name)
-      if previous_player.nil? || this_player.commit != previous_player.commit
-        this_player.relative_points
-      else
+      if previous_player.nil? || this_player.nil? || this_player.commit != previous_player.commit
         nil
+      else
+        this_player.relative_points
       end
     end
   end
